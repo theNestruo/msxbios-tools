3af3b7f0654875c237df9259ba56434331e55319
Néstor Sancho <theNestruo@gmail.com>
2025-03-16 09:25:02 +0100

    Code clean-up

--- src/main/java/com/github/thenestruo/msx/msxbiostools/MsxBiosToolsApp.java
+++ src/main/java/com/github/thenestruo/msx/msxbiostools/MsxBiosToolsApp.java
@@ -1,10 +1,10 @@
 package com.github.thenestruo.msx.msxbiostools;
 
 import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -12,17 +12,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.tinylog.Logger;
 
 import com.github.thenestruo.msx.msxbiostools.fields.BorderColor;
 import com.github.thenestruo.msx.msxbiostools.fields.CountryBasicVersion;
@@ -94,25 +94,25 @@ public class MsxBiosToolsApp {
 			Msx1HasSlotfix.INSTANCE
 		);
 
-	public static void main(String[] args) throws Exception {
+	public static void main(final String[] args) throws Exception {
 
 		// Parses command line
 		final Options options = options();
 		final CommandLine command = new DefaultParser().parse(options, args);
 
 		// Builds input file list
-		final List<File> inputFiles = new ArrayList<>();
-		final Queue<File> queue = new LinkedList<>();
-		for (String arg : command.getArgList()) {
-			queue.add(new File(arg));
+		final Queue<Path> queue = new LinkedList<>();
+		for (final String arg : command.getArgList()) {
+			queue.add(Path.of(arg));
 		}
+		final List<Path> inputFiles = new ArrayList<>();
 		while (!queue.isEmpty()) {
-			final File file = queue.poll();
-			if (FileUtils.isDirectory(file)) {
-				queue.addAll(FileUtils.listFiles(file,
-						FileFilterUtils.trueFileFilter(),
-						FileFilterUtils.falseFileFilter()));
-			} else if (file.canRead()) {
+			final Path file = queue.poll();
+			if (Files.isDirectory(file)) {
+				Files.list(file)
+						.filter(Predicate.not(Files::isDirectory))
+						.forEach((f) -> queue.add(f));
+			} else if (Files.isReadable(file)) {
 				inputFiles.add(file);
 			}
 		}
@@ -127,7 +127,7 @@ public class MsxBiosToolsApp {
 
 			final List<String> headers = new ArrayList<>();
 			headers.add("Filename");
-			for (Viewer viewer : TSV_VIEWERS) {
+			for (final Viewer viewer : TSV_VIEWERS) {
 				headers.add(viewer.getHeader());
 			}
 			final StringBuilder output = new StringBuilder();
@@ -137,28 +137,28 @@ public class MsxBiosToolsApp {
 							.create()
 							.setDelimiter('\t')
 							.setHeader(headers.toArray(new String[0]))
-							.build())) {
-				for (File inputFile : inputFiles) {
+							.get())) {
+				for (final Path inputFile : inputFiles) {
 					csvPrinter.printRecord(new MsxBiosToolsApp(inputFile)
 							.view(TSV_VIEWERS, true)
 							.getProperties()
 							.values());
 				}
 			}
-			System.out.println(output);
+			Logger.info(output);
 
 		} else {
 			// TEXT
 
-			for (File inputFile : inputFiles) {
-				for (Map.Entry<String, String> e :
-						new MsxBiosToolsApp(inputFile)
+			for (final Path inputFile : inputFiles) {
+				for (final Map.Entry<String, String> e
+						: new MsxBiosToolsApp(inputFile)
 							.view(TEXT_VIEWERS, false)
 							.getProperties()
 							.entrySet()) {
-					System.out.printf("%s: %s%n", e.getKey(), e.getValue());
+					Logger.info("{}: {}", e.getKey(), e.getValue());
 				}
-				System.out.println();
+				Logger.info("");
 			}
 		}
 	}
@@ -181,23 +181,23 @@ public class MsxBiosToolsApp {
 
 	private final Map<String, String> properties = new LinkedHashMap<>();
 
-	public MsxBiosToolsApp(final File file) {
+	public MsxBiosToolsApp(final Path file) {
 		super();
 
-		this.inputFilename = file.getAbsolutePath();
+		this.inputFilename = file.toAbsolutePath().toString();
 		this.properties.put("Filename", FilenameUtils.getBaseName(this.inputFilename));
 	}
 
-	public MsxBiosToolsApp view(List<Viewer> viewers, boolean includeEmpty) throws IOException {
+	public MsxBiosToolsApp view(final List<Viewer> viewers, final boolean includeEmpty) throws IOException {
 
 		final byte[] bios = new byte[0x8000];
-		try (final InputStream is = IOUtils.buffer(new FileInputStream(this.inputFilename))) {
+		try (final InputStream is = Files.newInputStream(Path.of(this.inputFilename))) {
 			IOUtils.readFully(is, bios, 0x0000, 0x8000);
-		} catch (EOFException e) {
+		} catch (final EOFException e) {
 			return this;
 		}
 
-		for (Viewer viewer : viewers) {
+		for (final Viewer viewer : viewers) {
 			if (viewer.canView(bios) || includeEmpty) {
 				this.properties.put(
 						viewer.getKey(),
